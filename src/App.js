4c9b988544ec141cf5ebd099e0c9d95d9ddda25e
gluay88 <p.wantagaew@gmail.comm>
2022-06-11 16:26:17 -0600

    aha

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Checkbox = ({ children }) => {
+  const [checked, setChecked] = useState(true);
+  const allChildren = React.Children.map(children, (child) => {
+    const clone = React.cloneElement(child, {
+      checked,
+      setChecked,
+    });
+    return clone;
+  });
+  return allChildren;
+};
+const CheckboxInput = ({ checked, setChecked }) => {
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => {
+          setChecked(e.target.checked);
+        }}
+      />
+    </>
+  );
+};
+
+const Label = ({ setChecked, children }) => {
+  return (
+    <label onClick={() => setChecked((state) => !state)}>{children}</label>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Checkbox>
+        <CheckboxInput />
+        <Label>Check box label</Label>
+      </Checkbox>
+    </div>
+  );
+}
+
+export default App;
